0f43d9e4a61e6ca0b2fc8d4e62418de3f914f798
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-06-15 21:53:03 +0900

    mySampleCode of ofVideoGrabber

---
+++ videoGrabberSample/src/ofApp.cpp
@@ -0,0 +1,142 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    w = 1280;
+    h = 720;
+    ofBackground(0);
+    ofSetFrameRate(60);
+    ofSetWindowTitle("ofVideoGrabber sample");
+    
+    video.initGrabber(w, h);
+    changedPixels = new unsigned char[w * h * 3];
+    
+    videoTexture.allocate(w, h, GL_RGB);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    video.update();
+    
+    if (video.isFrameNew()) {
+        unsigned char* pixels = video.getPixels().getData();
+        switch (state) {
+            case 1:
+                reverseColor(pixels);
+                break;
+            case 2:
+                noiseColor(pixels);
+                break;
+            default:
+                changedPixels = pixels;
+                break;
+        }
+        videoTexture.loadData(changedPixels, w, h, GL_RGB);
+    }
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    if (state == 0) {
+        video.draw(0, 0);
+    } else {
+        videoTexture.draw(0, 0);
+    }
+}
+
+//-------------------------------------------------------------
+void ofApp::reverseColor(unsigned char* pixels) {
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            // 赤
+            changedPixels[y * 3 * w + x * 3] = 255 - pixels[y * 3 * w + x * 3];
+            // 緑
+            changedPixels[y * 3 * w + x * 3 + 1] = 255 - pixels[y * 3 * w + x * 3 + 1];
+            // 青
+            changedPixels[y * 3 * w + x * 3 + 2] = 255 - pixels[y * 3 * w + x * 3 + 2];
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::noiseColor(unsigned char* pixels) {
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            // 赤
+            changedPixels[y * 3 * w + x * 3] = pixels[y * 3 * w + x * 3] + ofRandom(-30, -10);
+            // 緑
+            changedPixels[y * 3 * w + x * 3 + 1] = pixels[y * 3 * w + x * 3] + ofRandom(-30, -10);
+            // 青
+            changedPixels[y * 3 * w + x * 3 + 2] = pixels[y * 3 * w + x * 3] + ofRandom(-30, -10);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key) {
+        case '0':
+            state = 0;
+            break;
+        case '1':
+            state = 1;
+            break;
+        case '2':
+            state = 2;
+            break;
+    }
+    cout << state << endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
